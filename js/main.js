db4356fcbc6bb6d8477961a1e7de1343e2684df8
Mariano Salamanca Jr <msalamancajr@gmail.com>
2018-08-18 12:26:21 -0400

    Added second cat picture

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
-var cat = document.getElementById('catImg');
-var scoreText = document.getElementById('score');
-var score = 0;
+const cat = document.getElementById('catImg');
+const cat2 = document.getElementById('catImg2');
+const scoreText = document.getElementById('score');
+const scoreText2 = document.getElementById('score2');
+let score1 = 0;
+let score2 = 0;
 
 cat.addEventListener('click', function(){
-	score++;
-	scoreText.innerHTML = score;  
+	score1++;
+	scoreText.innerHTML = score1;  
+
+}, false);
+
+cat2.addEventListener('click', function(){
+	score2++;
+	scoreText2.innerHTML = score2;  
 
 }, false);
