e22c65d9a44db004ec7bc317a84575d772ec666f
Mariano Salamanca Jr <msalamancajr@gmail.com>
2018-08-26 15:21:54 -0400

    Need to figure out closure

--- js/main.js
+++ js/main.js
@@ -36,32 +36,54 @@ var octopus = {
 		return model.currentCat;
 	},
 
+	setCurrentCat: function(cat){
+		model.currentCat = cat;
+	},
+
 	incrementClick: function(cat){
 		cat.clickCount++;
+	},
+
+	renameCat: function(cat, catName){
+		cat.name = catName;
+	},
+
+	changeURL: function(cat, catURL){
+		cat.imgSrc = catURL;
+	},
+
+	changeClicks: function(cat, clicks){
+		cat.clickCount = clicks;
 	},
 
 	init: function(){
 		model.currentCat = model.cats[0];
 
-		view.init();
-		view.render(model.cats);
+		listView.init();
+		listView.render(model.cats);
+		displayView.init();
+		adminView.init();
 	}
 }
 
-var view = {
+var listView = {
 	init: function(){
 		this.catNameDiv = document.getElementById('displayCatName');
 		this.scoreDiv = document.getElementById('score'); 
 		this.listOfCats = document.getElementById('catList');
 		this.image = document.getElementsByTagName('img')[0];
+
 	},
 
 	render: function(catArray){
+
 		var listOfCats = this.listOfCats,
 			catNameDiv = this.catNameDiv,
 			scoreDiv = this.scoreDiv,
 			image = this.image;
 			currentCat = octopus.currentCat();
+
+		listOfCats.innerHTML = "";
 
 		for (let i = 0; i < catArray.length; i++){
 			let newLi = document.createElement("li");
@@ -75,12 +97,68 @@ var view = {
 			});
 		};
 
+		
+	}
+
+}
+
+var displayView = {
+	init: function(){
+		this.catNameDiv = document.getElementById('displayCatName');
+		this.scoreDiv = document.getElementById('score'); 
+		this.listOfCats = document.getElementById('catList');
+		this.image = document.getElementsByTagName('img')[0];
+
+		this.render();
+	},
+
+	render: function(){
+		var listOfCats = this.listOfCats,
+			catNameDiv = this.catNameDiv,
+			scoreDiv = this.scoreDiv,
+			image = this.image;
+			currentCat = octopus.currentCat();
+
 		image.addEventListener('click', function(){
 			octopus.incrementClick(currentCat);
 			scoreDiv.innerHTML = currentCat.clickCount;
-		})
+		});
 	}
+}
 
+var adminView = {
+	init: function(){
+		this.adminButton = document.getElementById('admin');
+		this.adminDisplay = document.getElementById('adminDisplay');
+		this.adminName = document.getElementById('adminName');
+		this.adminURL = document.getElementById('adminURL');
+		this.adminClicks = document.getElementById('adminClicks');
+		this.cancelButton = document.getElementById('cancel');
+		this.saveButton = document.getElementById('save');
+
+		this.render();
+	},
+
+	render: function(){
+		var adminButton = this.adminButton,
+			adminDisplay = this.adminDisplay,
+			saveButton = this.saveButton;
+			currentCat = octopus.currentCat();
+
+		adminButton.addEventListener('click', function(){
+			if(adminDisplay.hidden){
+				adminDisplay.hidden = false;
+			} else {
+				adminDisplay.hidden = true;
+			}
+		});
+
+		saveButton.addEventListener('click', function(){
+			octopus.renameCat(currentCat, adminName.value);
+			listView.render(model.cats);
+			// displayView.render();
+		});
+	}
 }
 
 octopus.init();
